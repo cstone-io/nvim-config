b7f41e91a4fffbd5d792dd743c09424501d08a6c
cstone-io <connor@stoneconsulting.io>
2023-10-28 18:26:27 -0400

    custom color for comments

--- lua/cstone/colorscheme.lua
+++ lua/cstone/colorscheme.lua
@@ -1,16 +1,20 @@
 local default = "dracula"
 
 function Recolor(color)
-  color = color or default
-  status_ok, _ = pcall(vim.cmd, "colorscheme " .. color)
-  if not status_ok then
-    return
-  end
+	color = color or default
+	local status_ok, _ = pcall(vim.cmd, "colorscheme " .. color)
+	if not status_ok then
+		return
+	end
 
-  vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
-  vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
-  vim.api.nvim_set_hl(0, "Pmenu", {bg = "none", fg = "#ffffff"})
-  vim.api.nvim_set_hl(0, "PmenuSel", {bg = "none", fg = "#ffffff"})
+	-- override window backgrounds
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "Pmenu", { bg = "none", fg = "#ffffff" })
+	vim.api.nvim_set_hl(0, "PmenuSel", { bg = "none", fg = "#ffffff" })
+
+	-- override comment colorscheme
+	vim.api.nvim_set_hl(0, "Comment", { fg = "#a5cce8" })
 end
 
 Recolor(default)
