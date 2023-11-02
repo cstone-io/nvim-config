4b28c90d98250384cddcf0738d0646cb25f3ce10
cstone-io <connor@stoneconsulting.io>
2023-11-02 13:21:40 -0400

    added custom bufferline naming behavior

--- lua/cstone/plugins/bufferline.lua
+++ lua/cstone/plugins/bufferline.lua
@@ -1,166 +1,208 @@
 local status_ok, bufferline = pcall(require, "bufferline")
 if not status_ok then
-  return
+	return
 end
 
-bufferline.setup {
-  options = {
-    numbers = "none", -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
-    close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
-    right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
-    left_mouse_command = "buffer %d", -- can be a string | function, see "Mouse actions"
-    middle_mouse_command = nil, -- can be a string | function, see "Mouse actions"
-    -- NOTE: this plugin is designed with this icon in mind,
-    -- and so changing this is NOT recommended, this is intended
-    -- as an escape hatch for people who cannot bear it for whatever reason
-	indicator_icon = nil,
-    indicator = { style = "icon", icon = "▎"},
-    buffer_close_icon = '',
-    modified_icon = "●",
-    close_icon = "",
-    left_trunc_marker = "",
-    right_trunc_marker = "",
-    --- name_formatter can be used to change the buffer's label in the bufferline.
-    --- Please note some names can/will break the
-    --- bufferline so use this at your discretion knowing that it has
-    --- some limitations that will *NOT* be fixed.
-    -- name_formatter = function(buf)  -- buf contains a "name", "path" and "bufnr"
-    --   -- remove extension from markdown files for example
-    --   if buf.name:match('%.md') then
-    --     return vim.fn.fnamemodify(buf.name, ':t:r')
-    --   end
-    -- end,
-    max_name_length = 30,
-    max_prefix_length = 30, -- prefix used when a buffer is de-duplicated
-    tab_size = 21,
-    diagnostics = false, -- | "nvim_lsp" | "coc",
-    diagnostics_update_in_insert = false,
-    -- diagnostics_indicator = function(count, level, diagnostics_dict, context)
-    --   return "("..count..")"
-    -- end,
-    -- NOTE: this will be called a lot so don't do any heavy processing here
-    -- custom_filter = function(buf_number)
-    --   -- filter out filetypes you don't want to see
-    --   if vim.bo[buf_number].filetype ~= "<i-dont-want-to-see-this>" then
-    --     return true
-    --   end
-    --   -- filter out by buffer name
-    --   if vim.fn.bufname(buf_number) ~= "<buffer-name-I-dont-want>" then
-    --     return true
-    --   end
-    --   -- filter out based on arbitrary rules
-    --   -- e.g. filter out vim wiki buffer from tabline in your work repo
-    --   if vim.fn.getcwd() == "<work-repo>" and vim.bo[buf_number].filetype ~= "wiki" then
-    --     return true
-    --   end
-    -- end,
-    offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
-    show_buffer_icons = true,
-    show_buffer_close_icons = true,
-    show_close_icon = true,
-    show_tab_indicators = true,
-    persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
-    -- can also be a table containing 2 custom separators
-    -- [focused and unfocused]. eg: { '|', '|' }
-    separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
-    enforce_regular_tabs = true,
-    always_show_bufferline = true,
-    -- sort_by = 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
-    --   -- add custom logic
-    --   return buffer_a.modified > buffer_b.modified
-    -- end
-  },
-  highlights = {
-    fill = {
-      fg = { attribute = "fg", highlight = "Visual" },
-      bg = { attribute = "bg", highlight = "TabLine" },
-    },
-    background = {
-      fg = { attribute = "fg", highlight = "TabLine" },
-      bg = { attribute = "bg", highlight = "TabLine" },
-    },
+-- function to filter buffers with the same name
+local function filter_buffers_with_same_name(target_name)
+	local buffers = vim.api.nvim_list_bufs()
+	local matches = {}
 
-    -- buffer_selected = {
-    --   fg = {attribute='fg',highlight='#ff0000'},
-    --   bg = {attribute='bg',highlight='#0000ff'},
-    --   gui = 'none'
-    --   },
-    buffer_visible = {
-      fg = { attribute = "fg", highlight = "TabLine" },
-      bg = { attribute = "bg", highlight = "TabLine" },
-    },
+	for _, buf in ipairs(buffers) do
+		if vim.api.nvim_buf_is_loaded(buf) then
+			local name = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(buf), ":t")
+			if name == target_name then
+				table.insert(matches, buf)
+			end
+		end
+	end
 
-    close_button = {
-      fg = { attribute = "fg", highlight = "TabLine" },
-      bg = { attribute = "bg", highlight = "TabLine" },
-    },
-    close_button_visible = {
-      fg = { attribute = "fg", highlight = "TabLine" },
-      bg = { attribute = "bg", highlight = "TabLine" },
-    },
-    -- close_button_selected = {
-    --   fg = {attribute='fg',highlight='TabLineSel'},
-    --   bg ={attribute='bg',highlight='TabLineSel'}
-    --   },
+	return matches
+end
 
-    tab_selected = {
-      fg = { attribute = "fg", highlight = "Normal" },
-      bg = { attribute = "bg", highlight = "Normal" },
-    },
-    tab = {
-      fg = { attribute = "fg", highlight = "TabLine" },
-      bg = { attribute = "bg", highlight = "TabLine" },
-    },
-    tab_close = {
-      -- fg = {attribute='fg',highlight='LspDiagnosticsDefaultError'},
-      fg = { attribute = "fg", highlight = "TabLineSel" },
-      bg = { attribute = "bg", highlight = "Normal" },
-    },
+-- function to find a unique tail for a buffer
+local function unique_tail_modifier(path, target_name)
+	local sep = package.config:sub(1, 1) -- Get the path separator for the current system
+	local parts = {}
+	for part in string.gmatch(path, "[^" .. sep .. "]+") do
+		table.insert(parts, part)
+	end
 
-    duplicate_selected = {
-      fg = { attribute = "fg", highlight = "TabLineSel" },
-      bg = { attribute = "bg", highlight = "TabLineSel" },
-      underline = true,
-    },
-    duplicate_visible = {
-      fg = { attribute = "fg", highlight = "TabLine" },
-      bg = { attribute = "bg", highlight = "TabLine" },
-      underline = true,
-    },
-    duplicate = {
-      fg = { attribute = "fg", highlight = "TabLine" },
-      bg = { attribute = "bg", highlight = "TabLine" },
-      underline = true,
-    },
+	-- Iterate backwards through path parts to find unique leading component
+	local unique_part = ""
+	for i = #parts - 1, 1, -1 do
+		unique_part = parts[i] .. sep .. unique_part
+		local other_path = vim.fn.findfile(target_name, unique_part .. "*")
+		if other_path == "" or not string.find(other_path, sep .. unique_part) then
+			break
+		end
+	end
+	return unique_part .. target_name
+end
 
-    modified = {
-      fg = { attribute = "fg", highlight = "TabLine" },
-      bg = { attribute = "bg", highlight = "TabLine" },
-    },
-    modified_selected = {
-      fg = { attribute = "fg", highlight = "Normal" },
-      bg = { attribute = "bg", highlight = "Normal" },
-    },
-    modified_visible = {
-      fg = { attribute = "fg", highlight = "TabLine" },
-      bg = { attribute = "bg", highlight = "TabLine" },
-    },
+bufferline.setup({
+	options = {
+		numbers = "none", -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
+		close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
+		right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
+		left_mouse_command = "buffer %d", -- can be a string | function, see "Mouse actions"
+		middle_mouse_command = nil, -- can be a string | function, see "Mouse actions"
+		-- NOTE: this plugin is designed with this icon in mind,
+		-- and so changing this is NOT recommended, this is intended
+		-- as an escape hatch for people who cannot bear it for whatever reason
+		indicator_icon = nil,
+		indicator = { style = "icon", icon = "▎" },
+		buffer_close_icon = "",
+		modified_icon = "●",
+		close_icon = "",
+		left_trunc_marker = "",
+		right_trunc_marker = "",
+		--- name_formatter can be used to change the buffer's label in the bufferline.
+		--- Please note some names can/will break the
+		--- bufferline so use this at your discretion knowing that it has
+		--- some limitations that will *NOT* be fixed.
+		name_formatter = function(buf) -- buf contains a "name", "path" and "bufnr"
+			-- Use the function defined above to get buffers with the same name
+			local matches = filter_buffers_with_same_name(buf.name)
+			if #matches > 1 then
+				-- More than one buffer with the same name, show unique part of the path
+				return unique_tail_modifier(buf.path, buf.name)
+			else
+				-- Only one buffer with this name, show just the name
+				return buf.name
+			end
+		end,
+		max_name_length = 30,
+		max_prefix_length = 30, -- prefix used when a buffer is de-duplicated
+		tab_size = 21,
+		diagnostics = false, -- | "nvim_lsp" | "coc",
+		diagnostics_update_in_insert = false,
+		-- diagnostics_indicator = function(count, level, diagnostics_dict, context)
+		--   return "("..count..")"
+		-- end,
+		-- NOTE: this will be called a lot so don't do any heavy processing here
+		-- custom_filter = function(buf_number)
+		--   -- filter out filetypes you don't want to see
+		--   if vim.bo[buf_number].filetype ~= "<i-dont-want-to-see-this>" then
+		--     return true
+		--   end
+		--   -- filter out by buffer name
+		--   if vim.fn.bufname(buf_number) ~= "<buffer-name-I-dont-want>" then
+		--     return true
+		--   end
+		--   -- filter out based on arbitrary rules
+		--   -- e.g. filter out vim wiki buffer from tabline in your work repo
+		--   if vim.fn.getcwd() == "<work-repo>" and vim.bo[buf_number].filetype ~= "wiki" then
+		--     return true
+		--   end
+		-- end,
+		offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
+		show_buffer_icons = true,
+		show_buffer_close_icons = true,
+		show_close_icon = true,
+		show_tab_indicators = true,
+		persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
+		-- can also be a table containing 2 custom separators
+		-- [focused and unfocused]. eg: { '|', '|' }
+		separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
+		enforce_regular_tabs = true,
+		always_show_bufferline = true,
+		-- sort_by = 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
+		--   -- add custom logic
+		--   return buffer_a.modified > buffer_b.modified
+		-- end
+	},
+	highlights = {
+		fill = {
+			fg = { attribute = "fg", highlight = "Visual" },
+			bg = { attribute = "bg", highlight = "TabLine" },
+		},
+		background = {
+			fg = { attribute = "fg", highlight = "TabLine" },
+			bg = { attribute = "bg", highlight = "TabLine" },
+		},
 
-    separator = {
-      fg = { attribute = "bg", highlight = "TabLine" },
-      bg = { attribute = "bg", highlight = "TabLine" },
-    },
-    separator_selected = {
-      fg = { attribute = "bg", highlight = "Normal" },
-      bg = { attribute = "bg", highlight = "Normal" },
-    },
-    -- separator_visible = {
-    --   fg = {attribute='bg',highlight='TabLine'},
-    --   bg = {attribute='bg',highlight='TabLine'}
-    --   },
-    indicator_selected = {
-      fg = { attribute = "fg", highlight = "LspDiagnosticsDefaultHint" },
-      bg = { attribute = "bg", highlight = "Normal" },
-    },
-  },
-}
+		-- buffer_selected = {
+		--   fg = {attribute='fg',highlight='#ff0000'},
+		--   bg = {attribute='bg',highlight='#0000ff'},
+		--   gui = 'none'
+		--   },
+		buffer_visible = {
+			fg = { attribute = "fg", highlight = "TabLine" },
+			bg = { attribute = "bg", highlight = "TabLine" },
+		},
+
+		close_button = {
+			fg = { attribute = "fg", highlight = "TabLine" },
+			bg = { attribute = "bg", highlight = "TabLine" },
+		},
+		close_button_visible = {
+			fg = { attribute = "fg", highlight = "TabLine" },
+			bg = { attribute = "bg", highlight = "TabLine" },
+		},
+		-- close_button_selected = {
+		--   fg = {attribute='fg',highlight='TabLineSel'},
+		--   bg ={attribute='bg',highlight='TabLineSel'}
+		--   },
+
+		tab_selected = {
+			fg = { attribute = "fg", highlight = "Normal" },
+			bg = { attribute = "bg", highlight = "Normal" },
+		},
+		tab = {
+			fg = { attribute = "fg", highlight = "TabLine" },
+			bg = { attribute = "bg", highlight = "TabLine" },
+		},
+		tab_close = {
+			-- fg = {attribute='fg',highlight='LspDiagnosticsDefaultError'},
+			fg = { attribute = "fg", highlight = "TabLineSel" },
+			bg = { attribute = "bg", highlight = "Normal" },
+		},
+
+		duplicate_selected = {
+			fg = { attribute = "fg", highlight = "TabLineSel" },
+			bg = { attribute = "bg", highlight = "TabLineSel" },
+			underline = true,
+		},
+		duplicate_visible = {
+			fg = { attribute = "fg", highlight = "TabLine" },
+			bg = { attribute = "bg", highlight = "TabLine" },
+			underline = true,
+		},
+		duplicate = {
+			fg = { attribute = "fg", highlight = "TabLine" },
+			bg = { attribute = "bg", highlight = "TabLine" },
+			underline = true,
+		},
+
+		modified = {
+			fg = { attribute = "fg", highlight = "TabLine" },
+			bg = { attribute = "bg", highlight = "TabLine" },
+		},
+		modified_selected = {
+			fg = { attribute = "fg", highlight = "Normal" },
+			bg = { attribute = "bg", highlight = "Normal" },
+		},
+		modified_visible = {
+			fg = { attribute = "fg", highlight = "TabLine" },
+			bg = { attribute = "bg", highlight = "TabLine" },
+		},
+
+		separator = {
+			fg = { attribute = "bg", highlight = "TabLine" },
+			bg = { attribute = "bg", highlight = "TabLine" },
+		},
+		separator_selected = {
+			fg = { attribute = "bg", highlight = "Normal" },
+			bg = { attribute = "bg", highlight = "Normal" },
+		},
+		-- separator_visible = {
+		--   fg = {attribute='bg',highlight='TabLine'},
+		--   bg = {attribute='bg',highlight='TabLine'}
+		--   },
+		indicator_selected = {
+			fg = { attribute = "fg", highlight = "LspDiagnosticsDefaultHint" },
+			bg = { attribute = "bg", highlight = "Normal" },
+		},
+	},
+})
